d77b387f05bae446395b6bbf4dbe76e3f5b8b39f
gabrieltakaki <gabitj@icloud.com>
2023-10-05 15:01:34 -0300

    fix: userservice

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,3 +1,20 @@
+import { IUser } from "partipro-shared/src/models/user/user.interface";
+import BadRequestError from "partipro-shared/src/errors/BadRequestError";
+
 import UserService from "partipro-shared/src/services/user.service";
 
-export default Object.assign({}, UserService);
+export default Object.assign({}, UserService, {
+  register: async (props: IUser) => {
+    const user = await UserService.findOne({
+      filters: {
+        email: props.email,
+      },
+    });
+
+    if (user) {
+      throw new BadRequestError("user_found", "Este email já existe");
+    }
+
+    return UserService.insert(props);
+  },
+});
