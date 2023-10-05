051a7c2cbe2a337c8fb287722a7e09117dbbb3a5
gabrieltakaki <gabitj@icloud.com>
2023-10-05 16:06:55 -0300

    test: jest config file

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,10 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+  setupFiles: ["dotenv/config"],
+  collectCoverage: true,
+  coverageReporters: ["text", "html"],
+  coverageDirectory: "<rootDir>/coverage/",
+  setupFilesAfterEnv: ["./shared/partipro-shared/__tests__/global.ts"],
+  testMatch: ["<rootDir>/__tests__/**/*.test.ts"],
 };
