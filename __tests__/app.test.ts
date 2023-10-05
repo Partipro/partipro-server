da00ba86f6f05f0db33e8dd60135b99edd6b4642
gabrieltakaki <gabitj@icloud.com>
2023-10-05 14:53:19 -0300

    test: jest initial configs

---
+++ __tests__/app.test.ts
@@ -0,0 +1,11 @@
+// @ts-ignore
+import request from "supertest";
+
+import app from "../src/app";
+
+describe("Test app.ts", () => {
+  test("Catch-all route", async () => {
+    const res = await request(app).get("/healthcheck");
+    expect(res.text).toEqual("Im Healthy");
+  });
+});
---
+++ __tests__/routes/authentication.route.test.ts
@@ -0,0 +1,17 @@
+// @ts-ignore
+import request from "supertest";
+
+import app from "../../src/app";
+
+describe("When /api/v1/auth/register", () => {
+  test("Should return status 200 and token when trying to register a user with correct information", async () => {
+    const res = await request(app).post("/api/v1/auth/register").send({
+      email: "test@jest.com",
+      password: "123",
+      name: "Jest Test",
+    });
+    expect(res.error).toBe("");
+    expect(res.status).toBe(201);
+    expect(res.body).toEqual(["Goon", "Tsuki", "Joe"]);
+  });
+});
