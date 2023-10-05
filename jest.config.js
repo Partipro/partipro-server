1974be7baad93f59437bfb445677b0d8c991731b
gabrieltakaki <gabitj@icloud.com>
2023-10-05 16:45:38 -0300

    fix: dotenv configs for tests

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  setupFiles: ["dotenv/config"],
   collectCoverage: true,
   coverageReporters: ["text", "html"],
   coverageDirectory: "<rootDir>/coverage/",
   setupFilesAfterEnv: ["./shared/partipro-shared/__tests__/global.ts"],
+  setupFiles: ["./shared/partipro-shared/__tests__/setupEnvs.ts"],
   testMatch: ["<rootDir>/__tests__/**/*.test.ts"],
 };
