47ed3145d83a8f5310125b5b9871c1f77fd526b2
gabrieltakaki <gabitj@icloud.com>
2023-10-05 19:24:01 -0300

    test: login route

--- __tests__/routes/authentication.route.test.ts
+++ __tests__/routes/authentication.route.test.ts
@@ -39,3 +39,47 @@ describe("When /api/v1/auth/register", () => {
     expect(res.body.error.message).toBe('"email" is required');
   });
 });
+
+describe("When /api/v1/auth", () => {
+  beforeEach(async () => {
+    await request(app).post("/api/v1/auth/register").send({
+      email: "test@jest.com",
+      password: "123",
+      name: "Jest Test",
+    });
+  });
+  test("Should return status 403 when trying to login a user with wrong password", async () => {
+    const res = await request(app).post("/api/v1/auth").send({
+      email: "test@jest.com",
+      password: "125633",
+    });
+    expect(res.body.error.message).toBe("Senha incorreta.");
+    expect(res.status).toBe(403);
+  });
+
+  test("Should return status 200 and token when trying to login with correct credentials", async () => {
+    const res = await request(app).post("/api/v1/auth").send({
+      email: "test@jest.com",
+      password: "123",
+    });
+    expect(res.status).toBe(200);
+    expect(res.body.token).toBeTruthy();
+  });
+
+  test("Should return status 400 when trying to login a email not existent", async () => {
+    const res = await request(app).post("/api/v1/auth").send({
+      email: "not@existing.com",
+      password: "123",
+    });
+    expect(res.status).toBe(404);
+    expect(res.body.error.message).toBe("Não foi possível encontrar este usuário.");
+  });
+
+  test("Should return status 400 when trying to login without providing a email", async () => {
+    const res = await request(app).post("/api/v1/auth").send({
+      password: "123",
+    });
+    expect(res.status).toBe(400);
+    expect(res.body.error.message).toBe('"email" is required');
+  });
+});
