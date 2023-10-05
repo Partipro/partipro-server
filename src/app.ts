dede08757271c92db8c273d6b11ce28d0042693f
gabrieltakaki <gabitj@icloud.com>
2023-10-05 14:52:16 -0300

    feat: it calls the router in the app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express, { Application, Request, Response } from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import router from "./routes";
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use("/api/v1", cors(), router);
 
 app.get("/healthcheck", (_request: Request, response: Response) => {
   response.status(200).send("Im Healthy");
