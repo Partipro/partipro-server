af695f846f484f8c48c334586af7565f44fbbcd6
gabrieltakaki <gabitj@icloud.com>
2023-10-09 22:02:43 -0300

    feat: request function with token to test protected endpoints

--- __tests__/request.ts
+++ __tests__/request.ts
@@ -1,5 +1,13 @@
-import { agent as supertest } from "superagent";
-import Jwt from "jsonwebtoken";
+import { agent as request } from "supertest";
 
 import app from "../src/app";
-import { IDS } from "partipro-shared/__tests__/setupData";
+
+export default async (method: "post" | "get" | "put" | "delete", endpoint: string) => {
+  const loginResponse = await request(app).post("/api/v1/auth/register").send({
+    email: "mocked@user.com",
+    password: "123",
+    name: "Jest Test",
+  });
+
+  return request(app)[method](`/api/v1/${endpoint}`).set("Cookie", `ccToken=${loginResponse.body.token}`);
+};
