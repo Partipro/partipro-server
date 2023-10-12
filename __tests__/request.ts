ed6e77060aaf62beb75e12863414e11d8749bf13
gabrieltakaki <gabitj@icloud.com>
2023-10-12 20:09:38 -0300

    fix: data to the test request.ts

--- __tests__/request.ts
+++ __tests__/request.ts
@@ -2,12 +2,12 @@ import { agent as request } from "supertest";
 
 import app from "../src/app";
 
-export default async (method: "post" | "get" | "put" | "delete", endpoint: string) => {
+export default async (method: "post" | "get" | "put" | "delete", endpoint: string, { data = {} }: { data?: any }) => {
   const loginResponse = await request(app).post("/api/v1/auth/register").send({
     email: "mocked@user.com",
     password: "123",
     name: "Jest Test",
   });
 
-  return request(app)[method](`/api/v1/${endpoint}`).set("Cookie", `ccToken=${loginResponse.body.token}`);
+  return request(app)[method](`/api/v1/${endpoint}`).set("Cookie", `ccToken=${loginResponse.body.token}`).send(data);
 };
