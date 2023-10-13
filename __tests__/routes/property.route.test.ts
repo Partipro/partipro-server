27633cbcbd079ad826c68a001bf3d76c51717780
gabrieltakaki <gabitj@icloud.com>
2023-10-12 22:04:51 -0300

    test: get properties endpoint

--- __tests__/routes/property.route.test.ts
+++ __tests__/routes/property.route.test.ts
@@ -1,6 +1,53 @@
 import request from "../request";
+import User from "partipro-shared/src/models/user/user.model";
+import Property from "partipro-shared/src/models/property/property.model";
+import { IDS } from "partipro-shared/__tests__/setupData";
 
-describe("When POST /api/v1/property", () => {
+describe("when GET /api/v1/properties", () => {
+  beforeEach(async () => {
+    const user1 = await new User({
+      name: "User 1",
+      password: "123",
+      email: "user1@test.com",
+    }).save();
+    await new Property({
+      owner: user1._id,
+      name: "Property of user 1",
+      deleted: false,
+      address: "There",
+    }).save();
+    await new Property({
+      owner: IDS.USER,
+      name: "Property of mine",
+      deleted: false,
+      address: "There",
+    }).save();
+    await new Property({
+      owner: IDS.USER,
+      name: "Property of mine 2",
+      address: "There",
+      deleted: false,
+    }).save();
+  });
+  it("Should status 200 and all the properties related to the user that is requesting", async () => {
+    const res = await request("get", "properties", {});
+
+    expect(res.status).toBe(200);
+    expect(res.body.length).toBe(2);
+    expect(res.body).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          name: "Property of mine",
+        }),
+        expect.objectContaining({
+          name: "Property of mine 2",
+        }),
+      ]),
+    );
+  });
+});
+
+describe("When POST /api/v1/properties", () => {
   test("Should return status 201 and the property when the sent data is correct", async () => {
     const res = await request("post", "properties", {
       data: {
