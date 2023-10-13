057ae83a7bb0f9f00000b1449736c5c34a5c48d8
gabrieltakaki <gabitj@icloud.com>
2023-10-12 21:47:52 -0300

    fix: request.ts user mocked login

--- __tests__/request.ts
+++ __tests__/request.ts
@@ -3,10 +3,9 @@ import { agent as request } from "supertest";
 import app from "../src/app";
 
 export default async (method: "post" | "get" | "put" | "delete", endpoint: string, { data = {} }: { data?: any }) => {
-  const loginResponse = await request(app).post("/api/v1/auth/register").send({
-    email: "mocked@user.com",
+  const loginResponse = await request(app).post("/api/v1/auth").send({
+    email: "user@jest.com",
     password: "123",
-    name: "Jest Test",
   });
 
   return request(app)[method](`/api/v1/${endpoint}`).set("Cookie", `ccToken=${loginResponse.body.token}`).send(data);
