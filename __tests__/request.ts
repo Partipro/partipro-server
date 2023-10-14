79a077630c204dfdb3d38eb72af55366decd81aa
gabrieltakaki <gabitj@icloud.com>
2023-10-14 09:56:54 -0300

    fix: data parameter with default value

--- __tests__/request.ts
+++ __tests__/request.ts
@@ -2,7 +2,11 @@ import { agent as request } from "supertest";
 
 import app from "../src/app";
 
-export default async (method: "post" | "get" | "put" | "delete", endpoint: string, { data = {} }: { data?: any }) => {
+export default async (
+  method: "post" | "get" | "put" | "delete",
+  endpoint: string,
+  { data = {} }: { data?: any } = {},
+) => {
   const loginResponse = await request(app).post("/api/v1/auth").send({
     email: "user@jest.com",
     password: "123",
